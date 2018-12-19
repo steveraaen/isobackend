ef19f31f0e4b3ca303fe73a057999eb47932cb8d
Steveraaen <sraaen@gmail.com>
2018-12-19 15:53:16 -0500

    added routes for nearby businesses

--- server.js
+++ server.js
@@ -16,28 +16,14 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // ------ Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-/*app.get('/hotels', function(req, res) {
-	 console.log(req.params);
-	client.search({
-	  term:'Marriot',
-	  location: 'Madrid, es'
-		})
-	.then(response => {
-			var val = response.jsonBody.businesses
-		 
-		  res.json(val)
-		})
-	.catch(e => {
-		  console.log(e);
-});
-})*/
+
 app.get('/hotels', function(req, res) {
-	 console.log(req.query.city, req.query.chain);
-	
+	 console.log(req.query.city, req.query.chain);	
 	client.search({
 	  term: JSON.stringify(req.query.chain),
 	  location: JSON.stringify(req.query.city),
-	  
+	  categories: 'hotels'
+
 		})
 	.then(response => {
 			var val = response.jsonBody.businesses
@@ -48,7 +34,27 @@ app.get('/hotels', function(req, res) {
 		  console.log(e);
 });
 })
- 
+app.get('/details', function(req, res) {
+	 console.log(req.query.longitude, req.query.latitude);
+
+	client.search({	  
+	  longitude: req.query.longitude,
+	  latitude: req.query.latitude,
+	  radius: 1000,
+	  limit: 50,
+	  categories: 'restaurants',
+	  rating: '3, 4, 5',
+	  sort_by: 'distance'
+		})
+	.then(response => {
+			var val = response.jsonBody.businesses
+		 
+		  res.json(val)
+		})
+	.catch(e => {
+		  console.log(e);
+});
+})
 const port = process.env.PORT || 5001;
 app.listen(port);
 console.log(`Listening on ${port}`);
