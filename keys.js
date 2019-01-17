86d85a5e88889cb5f46cb70acaee0846c02a3f83
Steveraaen <sraaen@gmail.com>
2019-01-17 16:40:58 -0500

    removed keys from gitignore

---
+++ keys.js
@@ -0,0 +1,8 @@
+const keys = {
+ mbk: "pk.eyJ1Ijoic3JhYWVuIiwiYSI6ImNqcGxudTVwaDBnNGQ0OW1sZ2NhcjVyZm8ifQ.J6sCIBi_knv8qU39mHqprA",
+ gk: "AIzaSyCwzMnUbKY1ayJZ_d26Vbhx5AHs_pb5u4o",
+ ylp: "LOoRmeEKyRpK6yyZEpWMYPSIhYUiKDKbNb3BrWJ7pvgT__JeSy4F6IPKBvAS0W3wZt35SPuMbf-WLCoW_GN34qje_hcicTGAj-8VBkY5l9NACu07UZIslUDZbRQUXHYx",
+ msqlkey: 'modernWater360'
+}
+
+module.exports = keys
